b0a05c87311ea4ace6ab61a93cc15e439fcefecf
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-04-16 19:51:29 +0200

    Create authValidator.ts

---
+++ src/utils/validator/authValidator.ts
@@ -0,0 +1,30 @@
+const { check } = require('express-validator');
+import validatorMiddleware from '../../middleware/validatorMiddleware';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const signupValidator = [
+    check("email")
+        .notEmpty()
+        .withMessage("email is required")
+        .isEmail()
+        .withMessage("provide a valid email")
+        .custom(async (value: string) => {
+            const userDoc = await prisma.user.findUnique({
+                where: { email: value }
+            });
+            if (userDoc) {
+                throw new Error("Email already in use");
+            }
+            return true;
+        }),
+
+    check("password")
+        .notEmpty()
+        .withMessage("password is required")
+        .isLength({ min: 6 })
+        .withMessage("password must be at least 6 characters"),
+
+    validatorMiddleware,
+];
