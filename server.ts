58bcfaacdefde404bedeabbfb90e4743ecd35514
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-04-16 19:51:46 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-
+import mainRoutes from './src/Routes/index'
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/api', mainRoutes);
 
 app.listen(3000, () => {
     console.log('Server up and running on port: 3000');
