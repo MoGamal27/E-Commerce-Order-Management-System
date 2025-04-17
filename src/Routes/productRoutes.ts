76a4ae0217e3a6c5f9020b016ee9e97d6b39f470
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-04-17 07:27:36 +0200

    feat: add product routes for CRUD operations

---
+++ src/Routes/productRoutes.ts
@@ -0,0 +1,13 @@
+import { createProduct, updateProduct, deleteProduct, getProduct, getProducts } from "../Controller/productController";
+import express from "express";
+import { productValidator } from "../utils/validator/productValidator";
+
+const router = express.Router();
+
+router.post('/create', productValidator, createProduct);
+router.put('/update/:id', productValidator, updateProduct);
+router.delete('/delete/:id', deleteProduct);
+router.get('/get/:id', getProduct);
+router.get('/get', getProducts);
+
+export default router;
