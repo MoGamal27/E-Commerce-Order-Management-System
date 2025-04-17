fa3181d9a141de7288edc670f9217db221683387
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-04-17 19:32:25 +0200

    Update index.ts

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -3,12 +3,14 @@ import  authRoutes  from "./authRoutes";
 import categoryRoutes from "./categoryRoutes"
 import productRoutes from "./productRoutes"
 import cartRoutes from "./cartRoutes"
+import orderRoutes from "./orderRoutes"
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/category", categoryRoutes);
 router.use("/product", productRoutes)
 router.use("/cart", cartRoutes)
+router.use("/order", orderRoutes)
 
 
 export default router;
