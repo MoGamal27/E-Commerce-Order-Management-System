dee3ec742f32d1422be91211c73a1456a02e4683
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-04-17 21:30:23 +0200

    protect create product route

--- src/Routes/productRoutes.ts
+++ src/Routes/productRoutes.ts
@@ -1,10 +1,12 @@
 import { createProduct, updateProduct, deleteProduct, getProduct, getProducts, getFilteredProducts } from "../Controller/productController";
+import verifyToken from "../middleware/verifyToken";
+import verifyRole from "../middleware/verifyRole";
 import express from "express";
 import { productValidator } from "../utils/validator/productValidator";
 
 const router = express.Router();
 
-router.post('/create', productValidator, createProduct);
+router.post('/create', productValidator, verifyToken, verifyRole("ADMIN"), createProduct);
 router.put('/update/:id', productValidator, updateProduct);
 router.delete('/delete/:id', deleteProduct);
 router.get('/get/:id', getProduct);
