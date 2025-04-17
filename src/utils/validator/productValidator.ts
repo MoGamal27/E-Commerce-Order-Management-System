05ded34cf8e79eafc748f1068db0e35ee8e65f52
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-04-17 07:28:02 +0200

    feat: add product validator for request validation

---
+++ src/utils/validator/productValidator.ts
@@ -0,0 +1,38 @@
+const { check } = require("express-validator");
+import validatorMiddleware from '../../middleware/validatorMiddleware'
+
+export const productValidator = [
+    check("title")
+        .notEmpty()
+        .withMessage("Title is required")
+        .isLength({ min: 3 })
+        .withMessage("Title must be at least 3 characters")
+        .isLength({ max: 32 })
+        .withMessage("Title must be less than 32 characters"),
+
+    check("description")
+        .notEmpty()
+        .withMessage("Description is required")
+        .isLength({ min: 3 })
+        .withMessage("Description must be at least 3 characters"),
+
+    check("price")
+        .notEmpty()
+        .withMessage("Price is required")
+        .isNumeric()
+        .withMessage("Price must be a number"),
+
+    check("quantity")
+        .notEmpty()
+        .withMessage("Quantity is required")
+        .isNumeric()
+        .withMessage("Quantity must be a number"),
+
+    check("categoryId")
+        .notEmpty()
+        .withMessage("Category ID is required")
+        .isNumeric()
+        .withMessage("Category ID must be a number"),
+
+    validatorMiddleware
+]
