8ae87e9d1112edc5f2758c063144a5ad6243171e
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-04-18 13:38:52 +0200

    feat(products): add bulk product creation endpoint

--- src/Routes/productRoutes.ts
+++ src/Routes/productRoutes.ts
@@ -1,4 +1,4 @@
-import { createProduct, updateProduct, deleteProduct, getProduct, getProducts, getFilteredProducts } from "../Controller/productController";
+import { createProduct, updateProduct, deleteProduct, getProduct, getProducts, getFilteredProducts, createBulkProducts } from "../Controller/productController";
 import verifyToken from "../middleware/verifyToken";
 import verifyRole from "../middleware/verifyRole";
 import express from "express";
@@ -7,6 +7,7 @@ import { productValidator } from "../utils/validator/productValidator";
 const router = express.Router();
 
 router.post('/create', productValidator, verifyToken, verifyRole("ADMIN"), createProduct);
+router.post('create-bulk', productValidator, verifyToken, verifyRole("ADMIN"), createBulkProducts)
 router.put('/update/:id', productValidator, updateProduct);
 router.delete('/delete/:id', deleteProduct);
 router.get('/get/:id', getProduct);
