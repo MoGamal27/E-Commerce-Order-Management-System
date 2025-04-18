6fb6596dc17d88d0a6acc12e0499a29d978c74f2
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-04-18 11:50:44 +0200

    feat(Rate limit): add middleware rate limit

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import mainRoutes from './src/Routes/index'
 import globalError from './src/middleware/errorMiddleware';
+import limiter from './src/middleware/rateLimit';
 const app = express();
 
 app.use(express.json());
@@ -9,6 +10,9 @@ app.use('/api', mainRoutes);
 
 // Global Error Handling Middleware For Express
 app.use(globalError);
+
+
+app.use(limiter)
 
 app.listen(3000, () => {
     console.log('Server up and running on port: 3000');
