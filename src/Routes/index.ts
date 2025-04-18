9595119cb4cbb964090d2f744f8ad52ea4b18d7d
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-04-18 22:23:34 +0200

    add dashboard route

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -5,6 +5,7 @@ import productRoutes from "./productRoutes"
 import cartRoutes from "./cartRoutes"
 import orderRoutes from "./orderRoutes"
 import paymentRoutes from "./paymentRoutes"
+import dashboardRoutes from "./dashboardRoutes"
 const router = Router();
 
 router.use("/auth", authRoutes);
@@ -13,6 +14,6 @@ router.use("/product", productRoutes)
 router.use("/cart", cartRoutes)
 router.use("/order", orderRoutes)
 router.use("/payment", paymentRoutes)
-
+router.use("/dashboard", dashboardRoutes)
 
 export default router;
